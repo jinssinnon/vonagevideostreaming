7dc6c514c5dbcbbbe43dce032632176992268589
jinssinnon <jins@sinnonteq.in>
2023-09-30 10:13:43 +0530

    demo screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { OTSession, OTPublisher, OTStreams, OTSubscriber } from 'opentok-react';
 
-function App() {
+const apiKey = '';
+const sessionId = '';
+const token = '';
+
+const App = () => {
+  useEffect(() => {
+    // Initialize Vonage Video session with your API credentials
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <OTSession apiKey={apiKey} sessionId={sessionId} token={token}>
+      {/* <OTPublisher /> */}
+      <OTStreams>
+        <OTSubscriber />
+      </OTStreams>
+    </OTSession>
+  </div>
   );
 }
 
